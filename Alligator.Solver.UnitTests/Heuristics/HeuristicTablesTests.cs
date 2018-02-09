ff0be16a4a69524ed93565dec672ba20c5d41c43
boraaros <bilics.adrian@gmail.com>
2018-02-09 18:35:54 +0100

    heuristic tables implementation

---
+++ Alligator.Solver.UnitTests/Heuristics/HeuristicTablesTests.cs
@@ -0,0 +1,146 @@
+﻿using Alligator.Solver.Heuristics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test.Heuristics
+{
+    [TestClass]
+    public class HeuristicTablesTests
+    {
+        private readonly IList<string> testMoves = Enumerable.Range(0, 3)
+            .Select(t => string.Format("testMove{0}", t)).ToList();
+
+        [TestMethod]
+        public void Heuristic_tables_store_and_return_killer_plies_with_same_depth()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            IList<string> result = heuristicTables.GetKillerPlies(1).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result.Contains(testMoves[0]));
+            Assert.IsTrue(result.Contains(testMoves[1]));
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_does_not_return_killer_plies_with_different_depth()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 2);
+            IList<string> result = heuristicTables.GetKillerPlies(1).ToList();
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.IsTrue(result.Contains(testMoves[0]));
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_return_only_last_two_stored_killer_plies()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[2], 1);
+            IList<string> result = heuristicTables.GetKillerPlies(1).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result.Contains(testMoves[1]));
+            Assert.IsTrue(result.Contains(testMoves[2]));
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_store_and_return_only_different_killer_plies()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            IList<string> result = heuristicTables.GetKillerPlies(1).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result.Contains(testMoves[0]));
+            Assert.IsTrue(result.Contains(testMoves[1]));
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_does_not_return_killer_plies_after_clear()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 1);
+            heuristicTables.ClearTables();
+            IList<string> result = heuristicTables.GetKillerPlies(1).ToList();
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_calculate_history_score()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[0], 2);
+            int result = heuristicTables.GetHistoryScore(testMoves[0]);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_does_not_update_history_score_with_another_ply()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.StoreBetaCutOff(testMoves[1], 2);
+            int result = heuristicTables.GetHistoryScore(testMoves[0]);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Heuristic_tables_return_zero_history_score_after_clear()
+        {
+            // Arrange
+            var heuristicTables = new HeuristicTables<string>();
+
+            // Act
+            heuristicTables.StoreBetaCutOff(testMoves[0], 1);
+            heuristicTables.ClearTables();
+            int result = heuristicTables.GetHistoryScore(testMoves[0]);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ Alligator.Solver/Heuristics/HeuristicTables.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alligator.Solver.Heuristics
+{
+    class HeuristicTables<TPly> : IHeuristicTables<TPly>
+    {
+        private readonly IDictionary<int, IList<TPly>> killerPlies;
+        private readonly IDictionary<TPly, int> historyScores;
+
+        private const int KillerPliesLimitPerDepth = 2;
+
+        public HeuristicTables()
+        {
+            killerPlies = new Dictionary<int, IList<TPly>>();
+            historyScores = new Dictionary<TPly, int>();
+        }
+
+        public void ClearTables()
+        {
+            killerPlies.Clear();
+            historyScores.Clear();
+        }
+
+        public void StoreBetaCutOff(TPly ply, int depth)
+        {
+            UpdateHistoryScores(ply, depth);
+            UpdateKillerPlies(ply, depth);
+        }
+
+        public int GetHistoryScore(TPly ply)
+        {
+            int result;
+            if (historyScores.TryGetValue(ply, out result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        public IEnumerable<TPly> GetKillerPlies(int depth)
+        {
+            IList<TPly> killers;
+            if (killerPlies.TryGetValue(depth, out killers))
+            {
+                return killers;
+            }
+            return Enumerable.Empty<TPly>();
+        }
+
+        public void UpdateHistoryScores(TPly ply, int depth)
+        {
+            var score = depth * depth;
+            int result;
+            if (historyScores.TryGetValue(ply, out result))
+            {
+                historyScores[ply] = result + score;
+            }
+            else
+            {
+                historyScores.Add(ply, score);
+            }
+        }
+
+        public void UpdateKillerPlies(TPly ply, int depth)
+        {
+            IList<TPly> killers;
+            if (killerPlies.TryGetValue(depth, out killers))
+            {
+                if (killers[0].Equals(ply))
+                {
+                    return;
+                }
+                killers.Insert(0, ply);
+                if (killers.Count > KillerPliesLimitPerDepth)
+                {
+                    killers.RemoveAt(KillerPliesLimitPerDepth);
+                }
+            }
+            else
+            {
+                killerPlies.Add(depth, new List<TPly> { ply });
+            }
+        }
+    }
+}
